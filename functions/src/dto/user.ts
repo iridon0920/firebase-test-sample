c43ca426795ec64f3a3e521196821f76bd424cd6
iridon0920 <iridon0920@gmail.com>
2020-11-18 14:35:31 +0000

    [追加] テスト対象プロダクションコード

---
+++ functions/src/dto/user.ts
@@ -0,0 +1,19 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  address: string;
+  likeCount: number;
+}
+
+export interface ReadPublicUser {
+  id: string;
+  name: string;
+  likeCount: number;
+}
+
+export interface ReadPrivateUser {
+  id: string;
+  email: string;
+  address: string;
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,48 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import { ReadPrivateUser, ReadPublicUser, User } from "./dto/user";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+admin.initializeApp();
+
+export const helloWorld = functions
+  .region("asia-northeast1")
+  .firestore.document("users/{uid}")
+  .onCreate((snap, context) => {
+    const data = snap.data();
+    const user: User = {
+      id: context.params.uid,
+      name: data.name,
+      email: data.email,
+      address: data.address,
+      likeCount: data.likeCount,
+    };
+
+    const privateUser: ReadPrivateUser = {
+      id: user.id,
+      email: user.email,
+      address: user.address,
+    };
+
+    const publicUser: ReadPublicUser = {
+      id: user.id,
+      name: user.name,
+      likeCount: user.likeCount,
+    };
+
+    return admin
+      .firestore()
+      .runTransaction(async (transaction) => {
+        await transaction.set(
+          admin.firestore().collection("read_private_users").doc(user.id),
+          privateUser
+        );
+        await transaction.set(
+          admin.firestore().collection("read_public_users").doc(user.id),
+          publicUser
+        );
+      })
+      .catch((error: any) => {
+        console.error(error);
+        throw error;
+      });
+  });
