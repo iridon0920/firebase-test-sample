476eecac199774d0bd780fa3b5a019646255f03e
iridon0920 <iridon0920@gmail.com>
2020-11-23 15:21:49 +0000

    [追加] firebase-functions-testパッケージを活用したテストパターン
    * プロダクションとテストで同じパッケージを共有していないとデータも共有されないため、functions以下に新たにjest環境を導入

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,48 +1,5 @@
-import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import { ReadPrivateUser, ReadPublicUser, User } from "./dto/user";
 
 admin.initializeApp();
 
-export const helloWorld = functions
-  .region("asia-northeast1")
-  .firestore.document("users/{uid}")
-  .onCreate((snap, context) => {
-    const data = snap.data();
-    const user: User = {
-      id: context.params.uid,
-      name: data.name,
-      email: data.email,
-      address: data.address,
-      likeCount: data.likeCount,
-    };
-
-    const privateUser: ReadPrivateUser = {
-      id: user.id,
-      email: user.email,
-      address: user.address,
-    };
-
-    const publicUser: ReadPublicUser = {
-      id: user.id,
-      name: user.name,
-      likeCount: user.likeCount,
-    };
-
-    return admin
-      .firestore()
-      .runTransaction(async (transaction) => {
-        await transaction.set(
-          admin.firestore().collection("read_private_users").doc(user.id),
-          privateUser
-        );
-        await transaction.set(
-          admin.firestore().collection("read_public_users").doc(user.id),
-          publicUser
-        );
-      })
-      .catch((error: any) => {
-        console.error(error);
-        throw error;
-      });
-  });
+export { onCreateUser } from "./onCreateUser";
