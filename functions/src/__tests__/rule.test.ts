8e30dc0b74071a206cef7742ec47987ea016377b
iridon0920 <iridon0920@gmail.com>
2020-11-26 14:31:11 +0000

    [追加] firestore背クリティルール設定及びテストコード

---
+++ functions/src/__tests__/rule.test.ts
@@ -0,0 +1,55 @@
+import * as firebase from "@firebase/rules-unit-testing";
+import { TokenOptions } from "@firebase/rules-unit-testing/dist/src/api";
+import { readFileSync } from "fs";
+
+const projectId = "rule-test";
+
+function getAuthFirestore(auth: TokenOptions) {
+  return firebase.initializeTestApp({ projectId, auth }).firestore();
+}
+
+describe("firestore セキュリティルールテスト", () => {
+  beforeAll(async () => {
+    await firebase.loadFirestoreRules({
+      projectId,
+      rules: readFileSync("../firestore.rules", "utf8"),
+    });
+  });
+
+  afterEach(async () => {
+    await firebase.clearFirestoreData({ projectId });
+  });
+
+  afterAll(async () => {
+    await Promise.all(firebase.apps().map((app) => app.delete()));
+  });
+
+  describe("ユーザ読み書きテスト", () => {
+    test("usersのデータは、ユーザ本人のみ書き込み可能", async () => {
+      const firestore = getAuthFirestore({ uid: "user" });
+      const ref = firestore.collection("users").doc("user");
+      await firebase.assertSucceeds(ref.set({ name: "サンプル" }));
+
+      const failsRef = firestore.collection("users").doc("userB");
+      await firebase.assertFails(failsRef.set({ name: "サンプル" }));
+    });
+
+    test("read_private_usersのデータは、ユーザ本人のみ読み込み可能", async () => {
+      const firestore = getAuthFirestore({ uid: "user" });
+      const ref = firestore.collection("read_private_users").doc("user");
+      await firebase.assertSucceeds(ref.get());
+
+      const failsRef = firestore.collection("read_private_users").doc("userB");
+      await firebase.assertFails(failsRef.get());
+    });
+
+    test("read_public_usersのデータは、認証済ユーザは誰でも閲覧可能", async () => {
+      const firestore = getAuthFirestore({ uid: "user" });
+      const ref = firestore.collection("read_public_users").doc("user");
+      await firebase.assertSucceeds(ref.get());
+
+      const failsRef = firestore.collection("read_public_users").doc("userB");
+      await firebase.assertSucceeds(failsRef.get());
+    });
+  });
+});
